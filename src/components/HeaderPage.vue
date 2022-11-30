a11eb58c69689e84b45bdcf12d073fca05676bc8
isma-91 <isma91@hotmail.it>
2022-11-30 14:30:29 +0100

    Add static Header style

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-
-  </div>
+  <header class="header">
+    <div class="logo">
+      <h1>BOOLFLIX</h1>
+    </div>
+    <label for="search">
+      <input type="text" placeholder="Cerca il tuo fil preferito...">
+      <button>Cerca</button>
+    </label>
+  </header>
 </template>
 
 <script>
@@ -11,5 +17,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+header {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  height: 100px;
+  background-color: black;
+    h1 {
+      color: red;
+    }
+}
 </style>
